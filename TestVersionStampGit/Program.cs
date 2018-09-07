70cf5ffb61f87b215bef4d84983bfeff96267218
jakubsuchybio <dev@jakubsuchy.com>
2018-09-07 21:35:15 +0200

    Add project files.

---
+++ TestVersionStampGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestVersionStampGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
